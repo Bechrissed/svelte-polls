7ad57240c3a83e466466cdaed3bade1e236c3a74
zephyrus21 <pandey.piyus@gmail.com>
2021-05-03 21:00:12 +0530

    adding button component

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,25 @@
 <script>
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
+  import CreatePollForm from './components/CreatePollForm.svelte';
   import Tabs from './shared/Tabs.svelte';
 
   let items = ['Current Polls', 'Add new Polls'];
   let activeItem = 'Current Polls';
+
+  const tabChangeHandler = (e) => {
+    activeItem = e.detail;
+  };
 </script>
 
 <Header />
 <main>
-  <Tabs {items} {activeItem} />
-  <h1>Hello Bitch!</h1>
+  <Tabs {items} {activeItem} on:tabChange={tabChangeHandler} />
+  {#if activeItem === 'Current Polls'}
+    <p>Current Polls</p>
+  {:else if activeItem === 'Add new Polls'}
+    <CreatePollForm />
+  {/if}
 </main>
 <Footer />
 
---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Button from '../shared/Button.svelte';
+  let fields = { question: '', answerA: '', answerB: '' };
+
+  const submitHandler = () => {
+    console.log(fields);
+  };
+</script>
+
+<form on:submit|preventDefault={submitHandler}>
+  <div class="form-field">
+    <label for="question">Poll Question: </label>
+    <input bind:value={fields.question} type="text" id="question" />
+  </div>
+  <div class="form-field">
+    <label for="answer-a">Answer A: </label>
+    <input bind:value={fields.answerA} type="text" id="answer-a" />
+  </div>
+  <div class="form-field">
+    <label for="answer-b">Answer B: </label>
+    <input bind:value={fields.answerB} type="text" id="answer-b" />
+  </div>
+  <Button inverse>Add Poll</Button>
+</form>
+
+<style>
+  form {
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .form-field {
+    margin: 18px auto;
+  }
+  input {
+    width: 100%;
+    border-radius: 6px;
+  }
+  label {
+    margin: 10px auto;
+    text-align: left;
+  }
+</style>
